ab9f8b15b6b2774f3b42a8718ac26b38ae7ad6d9
Cristian <cnovac1441@gmail.com>
2023-06-30 17:40:54 -0400

    next auth secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify";
 export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      secret: process.env.SECRET
-    }),
-    // ...add more providers here
-  ],
+    // Configure one or more authentication providers
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+        }),
+        // ...add more providers here
+    ],
+    secret: process.env.SECRET
 }
 export default NextAuth(authOptions)
